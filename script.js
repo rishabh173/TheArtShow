a37c0711e5ff890848c4782e251e054dd52fa577
rishabh173 <41022485+rishabh173@users.noreply.github.com>
2019-03-02 23:37:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+ 
+
+ $(window).on('load',function(){
+        $('.preloader').addClass('complete')
+    })
+$(function(){
+	
+  
+
+
+
+
+    //  $('#cover').hover(function() {
+        
+        
+    //     $(this).css('background-image','url(freedom.jpg)');
+    //     $('#company').css('color','black');
+    //     $('.navitems2').css('color','black');
+    //     $('.quote').css('color','black');
+
+        
+    // }, function() {
+       
+    //     $(this).css('background-image','url(bg.png)');
+    //      $('#company').css('color','#f6f6f6');
+    //     $('.navitems2').css('color','#f6f6f6');
+    //     $('.quote').css('color','#f6f6f6');
+    // });
+
+
+
+
+
+});
